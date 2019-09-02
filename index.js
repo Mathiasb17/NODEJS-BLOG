4008a597efefe3989fdb77e659007a91791490d7
Mathias BROUSSET <mathias.brousset@itron.com>
2019-09-02 13:33:51 +0200

    routes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use(expressEdge);
 app.set('views', `${__dirname}/views`);
 
-var routes = {
+let routes = {
     '/': 'index',
     '/index': 'index',
     '/about': 'about',
